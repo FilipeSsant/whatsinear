bc0bca2d200f5fc4aeefe544422c1d4fb0cea9d1
Filipe Santos <filipe.ssant@outlook.com>
2021-05-30 15:17:52 -0300

    add context spotify to button signIn

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -15,7 +15,7 @@ export function LoginButton() {
       <MdExitToApp size={16} color="#FFFFFF" opacity={0.6} />
     </Button>
   ) : (
-    <Button onClick={() => signIn()}>
+    <Button onClick={() => signIn('spotify')}>
       <FaSpotify size={24} color="#FFFFFF" />
       <span>Login with Spotify</span>
     </Button>
